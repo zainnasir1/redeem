625afb1dcfd0802ec676e419ab94994bc91703c9
zainnasir138 <zainnasir138@gmail.com>
2021-03-30 14:20:17 +0500

    popup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Mint from "./pages/mint/Mint";
 import Gallery from "./pages/gallery/Gallery";
 import Wallet from "./pages/wallet/Wallet";
 import GalleryDetail from "./pages/galleryDetail/GalleryDetail";
+import Popup from "./components/popup/Popup";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/mint" component={Mint} />
-        <Route exact path="/gallery" component={Gallery} />
-        <Route exact path="/wallet" component={Wallet} />
-        <Route exact path="/gallery-detail/:id" component={GalleryDetail} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/mint" component={Mint} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/wallet" component={Wallet} />
+          <Route exact path="/gallery-detail/:id" component={GalleryDetail} />
+        </Switch>
+      </Router>
+      <Popup />
+    </>
   );
 }
 
--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import "./Popup.css";
+import img1 from "../../assets/Home1/01Asset74.png";
+import img2 from "../../assets/Home1/01Asset75.png";
+import { BsBoxArrowInDownLeft } from "react-icons/bs";
+// import * as IoIcons from "react-icons/io";
+// import { IoListCircleOutline } from "react-icons/io";
 
 const Popup = () => {
-  const [mintingFee, setMintingFee] = useState();
-  const [coloringFee, setcoloringFee] = useState();
+  const [mintingFee, setMintingFee] = useState(3.0);
+  const [coloringFee, setcoloringFee] = useState(3.0);
   const [bandwMint, setBandwMint] = useState();
   const [coloredMinted, setColoredMinted] = useState();
   const [highestPrice, setHighestPrice] = useState();
@@ -11,27 +18,40 @@ const Popup = () => {
   return (
     <div
       style={{
-        backgroundColor: "red",
         position: "fixed",
         bottom: "10px",
         right: "4px",
-        height: "100px",
-        width: "100px",
       }}
     >
       <div style={{ position: "relative" }}>
-        <div
-          style={{
-            position: "absolute",
-            width: "150px",
-            height: "150px",
-            backgroundColor: "black",
-          }}
-        >
-          <div className="popup-head"></div>
+        <div className="popup">
+          <div className="popup-head">
+            <div
+              className="d-flex align-items-center "
+              style={{ justifyContent: "space-around" }}
+            >
+              <span>Statistics</span>
+              <img
+                src={img1}
+                alt="twit"
+                className="stats-img"
+                style={{ marginLeft: "1rem" }}
+              />
+              <img src={img2} alt="dots" className="stats-img" />
+              <BsBoxArrowInDownLeft className="stats-img" />
+            </div>
+          </div>
           <div className="popup-body">
             <div>
-              <span></span>
+              <div className="d-flex">
+                {/* <IoIcons.IoListCircleOutline className="mr-2" /> */}
+                <span>Minting fee</span>
+                <span>{mintingFee}&nbsp;ETH</span>
+              </div>
+              <div>
+                <span>Coloring fee</span>
+                <span>{coloringFee}&nbsp;ETH</span>
+              </div>
             </div>
           </div>
         </div>
